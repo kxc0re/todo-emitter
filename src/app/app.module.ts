f6e7905e07f1fe73d35818d709d8a1eae32191f7
Markert, Enrico <enrico.markert@hs-kempten.de>
2018-10-24 18:55:39 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsEmitPluginModule } from '@ngxs-labs/emitter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { TodoState } from './state/todo.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { FormsModule } from '@angular/forms';
+import { MatModule } from './mat.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgxsModule.forRoot([TodoState]),
+    NgxsEmitPluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    FormsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/state/todo.state.ts
@@ -0,0 +1,94 @@
+import { Todo } from '../models/todo.model';
+import { State, StateContext, Selector } from '@ngxs/store';
+import { Receiver, EmitterAction } from '@ngxs-labs/emitter';
+
+export interface TodoStateModel {
+  todos: Todo[];
+  selectedTodo: Todo;
+}
+
+const defaults: TodoStateModel = {
+  todos: [
+    { id: Math.random(), title: 'Test-Todo', completed: false },
+    { id: Math.random(), title: 'Todo2', completed: true }
+  ],
+  selectedTodo: null
+};
+
+@State({
+  name: 'TodoState',
+  defaults
+})
+export class TodoState {
+  @Selector()
+  static getTodos(state: TodoStateModel) {
+    return state.todos;
+  }
+
+  @Receiver()
+  public static toggleTodo({ getState, patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<Todo>) {
+    const state = getState();
+    patchState({
+      todos: state.todos.map((todo) => {
+        if (todo === payload) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  }
+
+  @Receiver()
+  public static selectionChanged({ patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<Todo[]>) {
+    patchState({
+      todos: payload
+    });
+  }
+
+  @Receiver()
+  public static editTodo({ patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<Todo>) {
+    patchState({
+      selectedTodo: payload
+    });
+  }
+
+  @Receiver()
+  public static completeTodo({ getState, patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<Todo>) {
+    const state = getState();
+    patchState({
+      todos: state.todos.map((todo) => {
+        if (todo === payload) {
+          todo.completed = true;
+        }
+        return todo;
+      })
+    });
+  }
+
+  @Receiver()
+  public static addTodo({ getState, patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<string>) {
+    const state = getState();
+    patchState({
+      todos: [...state.todos, { id: Math.random(), title: payload, completed: false }]
+    });
+  }
+
+  @Receiver()
+  public static removeTodo({ getState, patchState }: StateContext<TodoStateModel>, { payload }: EmitterAction<Todo>) {
+    const state = getState();
+    patchState({
+      todos: state.todos.filter((todo) => todo !== payload)
+    });
+  }
+
+  @Receiver()
+  public static completeAll({ getState, patchState }: StateContext<TodoStateModel>) {
+    const state = getState();
+    patchState({
+      todos: state.todos.map((todo) => {
+        todo.completed = true;
+        return todo;
+      })
+    });
+  }
+}
